a06f16d3feb9b8c14688e54b3d13a1ae32f80630
Qibin <qbye0405@gmail.com>
2022-05-16 22:11:58 -0400

    commit tree eac433e62c1ee454ec606a5d153bbbc436be809b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (start){
+    return Math.abs(start - 42)
+    }
+
+
+function distanceFromHqInFeet(start){
+    let blocks = distanceFromHqInBlocks(start);
+    return blocks * 264
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(end - start)*264
+}
+
+function calculatesFarePrice(start, end){
+    let i = distanceTravelledInFeet (start,end)
+    if (i <= 400) {
+        return 0
+    } else if (i <= 2000){
+        return (i - 400)* 0.02
+    } else if (i < 2500){
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+    }
